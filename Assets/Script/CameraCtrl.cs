1fe473ebe411272010bfc03d8a983202a8b8c25d
hyeanju <86582031+hyeanju@users.noreply.github.com>
2022-11-27 03:56:41 +0900

    Day 2
    카메라 회전 수정

---
+++ Assets/Script/CameraCtrl.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour
+{
+    private float rotSpeed = 5f;
+
+    private float MinX = -80f;
+    private float MaxX = 50;
+    private float rotX = 0f;
+    private float rotY = 0f;
+
+    public void UpdateRotate(float mouseX, float mouseY)
+    {
+        rotY += mouseX * rotSpeed;
+        rotX -= mouseY * rotSpeed;
+
+        rotX = ClampAngle(rotX, MinX, MaxX);
+        transform.rotation = Quaternion.Euler(rotX, rotY, 0);
+    }
+
+    private float ClampAngle(float angle, float min, float max)
+    {
+        if(angle < -360)
+        {
+            angle += 360;
+        }
+        if(angle > 360)
+        {
+            angle -= 360;
+        }
+
+        return Mathf.Clamp(angle, min, max);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/PlayerCtrl.cs
+++ Assets/Script/PlayerCtrl.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    public Camera main;
+
     private float h = 0.0f;
     private float v = 0.0f;
     private Transform tr;
-    public float moveSpeed = 10.0f;
-    public float rotSpeed = 100.0f;
+    public float moveSpeed = 10f;
+    private CameraCtrl cameractrl;
+    float mousex = 0f;
+    float mousey = 0f;
+
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        cameractrl = GetComponent<CameraCtrl>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -19,17 +31,24 @@ public class PlayerCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        PlayerMove();
+        UpdateRotate();
+    }
+
+    void PlayerMove()
+    {
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
 
-        Debug.Log("h=" + ((int)h).ToString());
-        Debug.Log("v=" + ((int)v).ToString());
-
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
-        //tr.Translate(Vector3.right * moveSpeed * h * Time.deltaTime, Space.Self);
         tr.Translate(moveDir * moveSpeed * Time.deltaTime, Space.Self);
+    }
 
-        tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+    void UpdateRotate()
+    {
+        mousex = Input.GetAxis("MouseX");
+        mousey = Input.GetAxis("MouseY");
 
+        cameractrl.UpdateRotate(mousex, mousey);
     }
 }
